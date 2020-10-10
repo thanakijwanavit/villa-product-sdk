7b279694135a2d3f7f7c8f1c155e1914bd3002ad
nic <nwanavit@hatari.cc>
2020-10-10 07:12:03 +0700

    completed initial sdk docs

--- villaInventorySdk/_nbdev.py
+++ villaInventorySdk/_nbdev.py
@@ -2,7 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"InventorySdk": "inventory.ipynb"}
+index = {"FunctionNames": "product-sdk.ipynb",
+         "ProductSdk": "product-sdk.ipynb"}
 
 modules = ["inventory.py"]
 
--- villaProductSdk/inventory.py
+++ villaProductSdk/inventory.py
@@ -6,6 +6,7 @@ __all__ = ['FunctionNames', 'ProductSdk']
 from botocore.config import Config
 from s3bz.s3bz import S3, Requests
 from lambdasdk.lambdasdk import Lambda
+from .schema import Event, Response
 import bz2, json, boto3, base64, logging
 logging.basicConfig(level=logging.INFO)
 
@@ -17,7 +18,7 @@ class FunctionNames:
   dumpToS3 = lambda self: f'product-dump-s3-{self.branchName}'
   updateProduct = lambda self: f'product-update-{self.branchName}'
   updateS3 = lambda self: f'product-update-s3-{self.branchName}'
-  singleQuery = lambda self: f'product-update-s3-{self.branchName}'
+  singleQuery = lambda self: f'product-get-{self.branchName}'
   allQuery = lambda self: f'product-get-all-{self.branchName}'
   inputBucket = lambda self: f'input-product-bucket-{self.branchName}'
   inventoryBucket = lambda self: f'product-bucket-{self.branchName}'
@@ -32,6 +33,16 @@ class ProductSdk:
     self.lambdaClient = Lambda(user =user, pw=pw, region = region)
     self.user = user; self.pw = pw; self.region = region
 
+  @staticmethod
+  def returnLambdaResponse(lambdaResponse:dict):
+    try:
+      return Response.fromDict(lambdaResponse).body
+    except:
+      logging.exception('error parsing body, perhaps there is no body in response')
+      logging.error(lambdaResponse)
+  @staticmethod
+  def printFirst(inputDict:dict):
+    return next(iter(inputDict.items()))
 
   def updateWithS3(self, data,
                    inputKeyName = 'input-data-name',
@@ -53,20 +64,37 @@ class ProductSdk:
     logging.info(f'data is saved to s3, invoking ingestion function')
 
     # call lambda function
-    lambdaPayload = {
-        'key': inputKeyName
-    }
-    logging.info(f'input to lambda is {lambdaPayload}')
-    return self.lambdaClient.invoke(
+    inputValue = Event(body = json.dumps({ 'key': inputKeyName })).to_dict()
+    logging.info(f'input to lambda is {inputValue}')
+    lambdaResponse = self.lambdaClient.invoke(
       functionName= functionName ,
-      input=lambdaPayload,
+      input=inputValue,
       invocationType= invocationType )
+    return self.returnLambdaResponse(lambdaResponse)
 
   def querySingleProduct(self, iprcode = '0171670', user=None, pw=None):
     '''query a single product'''
     #extract function name
     functionName = self.functionNames.singleQuery()
+    query = {'iprcode': iprcode}
+    inputValue = Event(body = json.dumps(query)).to_dict()
+    logging.info(inputValue)
+    lambdaResponse = self.lambdaClient.invoke(
+        functionName = functionName , input = inputValue )
+    return self.returnLambdaResponse(lambdaResponse)
 
-    input = { 'iprcode': iprcode }
-    return self.lambdaClient.invoke(
-        functionName = functionName, input = input )
+  def allQuery(self):
+    functionName = self.functionNames.allQuery()
+    lambdaResponse = self.lambdaClient.invoke(
+      functionName = functionName, input = {}
+    )
+    url = Response.fromDict(lambdaResponse).body['url']
+    result = Requests.getContentFromUrl(url)
+    return result
+  def syncS3(self):
+    '''force s3 to sync with the newly input data'''
+    functionName = self.functionNames.dumpToS3()
+    lambdaResponse = self.lambdaClient.invoke(
+      functionName = functionName, input = {}
+    )
+    return lambdaResponse
